934e4393216107e7cda70e8edfd130b24505b73b
yingzi01 <463712786@qq.com>
2015-08-14 10:04:14 +0800

    添加工程

---
+++ Github仓库/Github仓库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github仓库
+//
+//  Created by lanou on 15/8/14.
+//  Copyright (c) 2015年 颖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github仓库/Github仓库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github仓库
+//
+//  Created by lanou on 15/8/14.
+//  Copyright (c) 2015年 颖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
